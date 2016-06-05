b2e7b0232048df71db7080a9861fa08b46f6168f
Steven Moy <gThrive@stevenmoy.com>
2016-06-05 07:30:02 -0700

    Add test to illustrate SecKeyChain.Update issue

---
+++ SecKeyChain/SecKeyChain/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using Foundation;
+using UIKit;
+
+using Xunit.Runner;
+using Xunit.Sdk;
+using System.Reflection;
+
+namespace SecKeyChainTest
+{
+  // The UIApplicationDelegate for the application. This class is responsible for launching the
+  // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+  [Register("AppDelegate")]
+  public class AppDelegate : RunnerAppDelegate
+  {
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+      // We need this to ensure the execution assembly is part of the app bundle
+      AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
+
+
+      // tests can be inside the main assembly
+      AddTestAssembly(Assembly.GetExecutingAssembly());
+      // otherwise you need to ensure that the test assemblies will 
+      // become part of the app bundle
+      //AddTestAssembly(typeof(PortableTests).Assembly);
+
+      return base.FinishedLaunching(app, options);
+    }
+  }
+}
+
+
---
+++ SecKeyChain/SecKeyChain/Test.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Xunit;
+using Security;
+using Foundation;
+
+namespace SecKeyChainTest
+{
+  public class Test
+  {
+    const string TestAccountName = "this is a test record";
+    static readonly NSData TestSecret;
+    static readonly NSData TestNewSecret;
+
+    static Test()
+    {
+      TestSecret = NSData.FromString("this is my secret", NSStringEncoding.UTF8);
+      TestNewSecret = NSData.FromString("this is my new secret", NSStringEncoding.UTF8);
+    }
+
+    public Test()
+    {
+    }
+
+    [Fact]
+    public void TestUpdate()
+    {
+      var queryRecord = new SecRecord(SecKind.GenericPassword)
+      {
+          Account = TestAccountName,
+        Accessible = SecAccessible.WhenUnlocked
+      };
+
+      SecStatusCode queryStatusCode;
+      SecRecord queryResult;
+      queryResult = SecKeyChain.QueryAsRecord(queryRecord, out queryStatusCode);
+      if (queryStatusCode == SecStatusCode.ItemNotFound)
+      {
+        var newRecord = new SecRecord(SecKind.GenericPassword)
+        {
+          Account = TestAccountName,
+          Accessible = SecAccessible.WhenUnlocked,
+          Generic = TestSecret
+        };
+        var newRecordStatusCode = SecKeyChain.Add(newRecord);
+        Assert.True(newRecordStatusCode == SecStatusCode.Success);
+      }
+
+      queryResult = SecKeyChain.QueryAsRecord(queryRecord, out queryStatusCode);
+      Assert.True(queryStatusCode == SecStatusCode.Success);
+      Assert.True(TestSecret.Equals(queryResult.Generic));
+
+      // try updating
+      SecRecord newSecret = new SecRecord(SecKind.GenericPassword)
+      {
+          Generic = TestNewSecret
+      };
+      var updateStatusCode = SecKeyChain.Update(queryRecord, newSecret);
+      Assert.True(updateStatusCode == SecStatusCode.Success,
+        string.Format("Got {0} instead", updateStatusCode));
+
+      queryResult = SecKeyChain.QueryAsRecord(queryRecord, out queryStatusCode);
+      Assert.True(queryStatusCode == SecStatusCode.Success);
+      Assert.True(TestNewSecret.Equals(queryResult.Generic));
+    }
+  }
+}
+
